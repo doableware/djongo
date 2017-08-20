d74461cccfe4df42ce1a57033561e86b8c1e29c4
nesdis <nesdis@gmail.com>
2017-08-20 11:42:02 +0530

    Minimum python version specified

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from distutils.core import setup
 
 setup(
     name='djongo',
-    version='1.1',
+    version='1.2.2',
     packages=['djongo'],
     url='https://github.com/nesdis/djongo',
     license='BSD',
     author='nesdis',
     author_email='nesdis@gmail.com',
-    description='Driver for allowing Django to use NoSQL databases'
+    description='Driver for allowing Django to use NoSQL databases',
+	install_requires=[
+          'sqlparse>=0.2.3',
+		  'pymongo>=3.2.0'
+      ],
+	 python_requires='>=3.3'
 )
