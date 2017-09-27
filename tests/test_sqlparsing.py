68b3c8b48ec50038197445d0fe7a377f6bb8bd62
nesdis <nesdis@gmail.com>
2017-09-27 10:40:06 +0530

    Updates to fixes

--- tests/test_sqlparsing.py
+++ tests/test_sqlparsing.py
@@ -38,6 +38,7 @@ sql = 'SELECT "django_admin_log"."id", "django_admin_log"."action_time",\
    WHERE "django_admin_log"."user_id" = %(0)s ORDER BY "django_admin_log"."action_time" DESC LIMIT 10'
 
 sql = 'SELECT "auth_permission"."id", "auth_permission"."name", "auth_permission"."content_type_id", "auth_permission"."codename" FROM "auth_permission" INNER JOIN "auth_user_user_permissions" ON ("auth_permission"."id" = "auth_user_user_permissions"."permission_id") INNER JOIN "django_content_type" ON ("auth_permission"."content_type_id" = "django_content_type"."id") WHERE "auth_user_user_permissions"."user_id" = %s ORDER BY "django_content_type"."app_label" ASC, "django_content_type"."model" ASC, "auth_permission"."codename" ASC'
+
 sql = 'SELECT "auth_permission"."id", "auth_permission"."name", "auth_permission"."content_type_id", "auth_permission"."codename", "django_content_type"."id", "django_content_type"."app_label", "django_content_type"."model" FROM "auth_permission" INNER JOIN "django_content_type" ON ("auth_permission"."content_type_id" = "django_content_type"."id") ORDER BY "django_content_type"."app_label" ASC, "django_content_type"."model" ASC, "auth_permission"."codename" ASC'
 db = MongoClient()['django-db']
 test = Parse(db, sql, [1, 2, 3, 4, 5])
