ce3a19d50b81c822ad058f44c08299ba7b7d4a63
nesdis <nesdis@gmail.com>
2017-10-07 20:49:54 +0530

    Refactoring and bug fixes

--- setup.py
+++ setup.py
@@ -1,8 +1,49 @@
 from distutils.core import setup
 
+LONG_DESCRIPTION = """
+djongo
+Driver for allowing Django to use MongoDB as the database backend.
+
+Use Mongodb as a backend database for your django project, without changing a single django model!!!
+
+Usage:
+
+pip install djongo 
+Into settings.py file of your project, add: 
+
+``` 
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'your-db-name',
+    }
+}
+```
+  
+    Run manage.py migrate(ONLY the first time to create collections in mongoDB) 
+    YOUR ARE SET! HAVE FUN! 
+
+Requirements:
+
+  1. djongo requires python 3.5 or above.
+
+
+How it works:
+djongo is a SQL to mongodb query compiler. It translates a SQL query string into a mongoDB query document. As a result, all Django features, models etc work as is.
+  
+  Django contrib modules: 
+ 
+'django.contrib.admin',
+'django.contrib.auth',    
+'django.contrib.sessions',
+
+ and others... fully supported.
+ 
+ Full Documentation: 'https://nesdis.github.io/djongo/'
+"""
 setup(
     name='djongo',
-    version='1.2.3',
+    version='1.2.4',
     packages=['djongo'],
     url='https://nesdis.github.io/djongo/',
     license='BSD',
@@ -14,5 +55,6 @@ setup(
         'pymongo>=3.2.0',
         'django>=1.8'
     ],
+	long_description=LONG_DESCRIPTION,
     python_requires='>=3.5'
 )
