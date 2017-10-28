fdf082f2508f5efa96a3992e591e6d859f3b974b
nesdis <nesdis@users.noreply.github.com>
2017-10-28 22:32:02 +0530

    Fix for when returning non existent fields, still needs admin testing. (#16)

--- tests/dummy_app/dummy/models.py
+++ tests/dummy_app/dummy/models.py
@@ -1,8 +1,15 @@
-from django.db import models
-
+from djongo import models
 
 # Create your models here.
+
+class Embedded(models.Model):
+    text = models.CharField(max_length=100)
+
+    class Meta:
+        abstract = True
+
+
 class Dummy(models.Model):
 
     test = models.CharField(max_length=10)
-
+    embedded = models.EmbeddedModelField(model_container=Embedded)
