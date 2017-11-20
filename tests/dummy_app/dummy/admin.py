18cd8f8326a4fa357eedf467b74118d6d226d274
nesdis <nesdis@gmail.com>
2017-11-20 07:05:42 +0530

    Fix issue with admin interface for using embedded documents

--- tests/dummy_app/dummy/admin.py
+++ tests/dummy_app/dummy/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-# from .models import Dummy
+from .models import Dummy, Dummies, Dummy2
 # Register your models here.
-# admin.site.register(Dummy)
+admin.site.register(Dummy)
+admin.site.register(Dummy2)
+admin.site.register(Dummies)
