a86b9400055f9d2be9fdfaf219fcbf66dc815f5f
nesdis <nesdis@gmail.com>
2017-12-06 10:19:24 +0530

    New version 1.1.10

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ Important links
 
 setup(
     name='djongo',
-    version='1.2.9',
+    version='1.2.10',
     packages=['djongo'],
     url='https://nesdis.github.io/djongo/',
     license='BSD',
