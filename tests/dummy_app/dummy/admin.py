8f791b42d7588f9896cdc147ddb5b4569925463f
nesdis <nesdis@gmail.com>
2018-01-20 18:19:45 +0530

    Test for nested queries aliased fields

--- tests/dummy_app/dummy/admin.py
+++ tests/dummy_app/dummy/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 # from .models import BlogPost, MultipleBlogPosts, Dummy2
 # from .basic_embedded_models import Author, Entry
-from .basic_embedded_models import Author, Entry
+#from .basic_embedded_models import Author, Entry
 
 # Register your models here.
 # admin.site.register(BlogPost)
 # admin.site.register(Dummy2)
 # admin.site.register(MultipleBlogPosts)
 
-admin.site.register(Author)
-admin.site.register(Entry)
+#admin.site.register(Author)
+#admin.site.register(Entry)
