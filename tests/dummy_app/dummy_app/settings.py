22c8ec6bf331440df37491f121ef9f319fe29ee4
nesdis <nesdis@gmail.com>
2018-01-27 15:48:48 +0530

    Google Group discussion

--- tests/dummy_app/dummy_app/settings.py
+++ tests/dummy_app/dummy_app/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['localhost']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.djongo',
+        'ENGINE': 'djongo',
         # 'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'djongo-test',
         'USER': 'user',
