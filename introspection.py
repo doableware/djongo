17461f7eaae50455527a6ef2c5a08be82915e48c
Rick van Hattem <wolph@wol.ph>
2018-04-08 08:41:51 +0200

    Added support for automatic introspection to easily generate models from existing databases (#95)
    * added introspection
    
    * disabled json field since there is nothing resembling a dictfield for now

--- introspection.py
+++ introspection.py
@@ -1,12 +1,36 @@
-from django.db.backends.base.introspection import BaseDatabaseIntrospection, TableInfo
+import bson
+import datetime
+import collections
+
+from django.db.backends.base import introspection
 
 
-class DatabaseIntrospection(BaseDatabaseIntrospection):
+class DatabaseIntrospection(introspection.BaseDatabaseIntrospection):
+    SAMPLE_SIZE = 1000
+    TYPE_MAPPING = {
+        int: bson.int64.Int64,
+    }
+
+    data_types_reverse = {
+        int: 'IntegerField',
+        bson.int64.Int64: 'BigIntegerField',
+        bson.objectid.ObjectId: 'ObjectIdField',
+        # collections.OrderedDict: 'JSONField',
+        bool: 'BooleanField',
+        datetime.datetime: 'DateTimeField',
+        datetime.date: 'DateField',
+        float: 'FloatField',
+        list: 'ListField',
+        str: 'CharField',
+        'text': 'TextField',
+    }
+
     # def table_names(self, cursor=None, include_views=False):
     #     return sorted(cursor.m_cli_connection.collection_names(False))
 
     def get_table_list(self, cursor):
-        return [TableInfo(c,'t') for c in cursor.db_conn.collection_names(False)]
+        return [introspection.TableInfo(c, 't')
+                for c in cursor.db_conn.collection_names(False)]
 
     def get_constraints(self, cursor, table_name):
         constraint = {}
@@ -17,7 +41,8 @@ class DatabaseIntrospection(BaseDatabaseIntrospection):
                 continue
 
             columns = [field[0] for field in info['key']]
-            orders = ['ASC' if field[1] == 1 else 'DESC' for field in info['key']]
+            orders = ['ASC' if field[1] == 1 else 'DESC'
+                      for field in info['key']]
             constraint[name] = {
                 'columns': columns,
                 'primary_key': name == '__primary_key__',
@@ -34,3 +59,71 @@ class DatabaseIntrospection(BaseDatabaseIntrospection):
 
     def get_indexes(self, cursor, table_name):
         return self.get_constraints(cursor, table_name)
+
+    def get_relations(self, cursor, table_name):
+        return []
+
+    def get_table_description(self, cursor, table_name):
+        colspecs = collections.defaultdict(lambda: dict(
+            types=collections.Counter(),
+            specs=collections.defaultdict(int),
+        ))
+
+        results = cursor.db_conn[table_name].aggregate([
+            {'$sample': {'size': self.SAMPLE_SIZE}},
+        ])
+        for result in results:
+            for k, v in result.items():
+                column = colspecs[k]
+                specs = column['specs']
+
+                column['types'][type(v)] += 1
+
+                if isinstance(v, str):
+                    specs['length'] = max(specs['length'], len(str(v)))
+
+                if isinstance(v, (int, bson.int64.Int64, float)):
+                    specs['max_value'] = max(specs['max_value'], v)
+
+                if isinstance(v, float):
+                    # Convert to string and count the characters after the .
+                    precision = len(str(v).split('.')[1])
+                    specs['precision'] = max(specs['precision'], precision)
+
+        columns = []
+        for name, column in colspecs.items():
+            types = column['types']
+            specs = column['specs']
+
+            if type(None) in types:
+                nullable = True
+                del types[type(None)]
+            else:
+                nullable = False
+
+            for from_, count in list(types.items()):
+                to = self.TYPE_MAPPING.get(from_)
+                if to:
+                    types[to] = types.pop(from_)
+
+            type_ = types.most_common(1)[0][0]
+            if type_ == str and specs['length'] > 200:
+                type_ = 'text'
+                del specs['length']
+
+            if len(types) > 1:
+                print('# Found multiple types for %s.%s: %s' %
+                      (table_name, name, types))
+
+            columns.append(introspection.FieldInfo(
+                name,  # name
+                type_,  # type_code
+                specs.get('length'),  # display size
+                specs.get('length'),  # internal size
+                specs.get('precision'),  # precision
+                None,  # scale
+                nullable,  # nullable
+                None,  # default
+            ))
+
+        return columns
