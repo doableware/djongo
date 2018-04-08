8ac76249afffc7cb6266096421d376ae40cde542
Sergey Shishov <sshishov@users.noreply.github.com>
2018-04-08 10:44:09 +0400

    Add support for UUID field to store in binary format (#99)
    Thank you for your contribution

--- djongo/features.py
+++ djongo/features.py
@@ -5,3 +5,4 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     supports_transactions = False
     # djongo doesn't seem to support this currently
     has_bulk_insert = False
+    has_native_uuid_field = True
