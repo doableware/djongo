d403cbe0045c4e7fa11d6f52600192beb6951b62
nesdis <nesdis@gmail.com>
2018-04-13 22:56:21 +0530

    API docs

--- docs/_apidocs/_build/_static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '',
-    LANGUAGE: 'None',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
-};
