9868b298a9591de4b5069f3bcd6084f97292c1b3
nesdis <nesdis@gmail.com>
2019-04-14 10:49:06 +0200

    Credits to contributors

--- djongo/__init__.py
+++ djongo/__init__.py
@@ -1,2 +1,11 @@
+# This version of Djongo was made possible by
+# the generous contributions from:
+#
+#       * TechDragon
+#       * Zachary Sizemore
+#       * Wayne Van Son
+#       * Norman Niemer
+#       * Renz Ladia
+#       * thestick613
 
 __version__ = '1.2.32'
