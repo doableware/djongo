59e812dc814bc11e60b5ef78cbc9ba0ecddb5544
nesdis <nesdis@gmail.com>
2019-08-30 23:12:51 +0200

    Test improvements

--- tests/django_tests/tests_old/check_framework/urls/name_with_colon.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import url
-
-urlpatterns = [
-    url(r'^$', lambda x: x, name='name_with:colon'),
-]
--- tests/django_tests/tests_old/check_framework/urls/no_warnings.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-
-urlpatterns = [
-    url(r'^foo/', lambda x: x, name='foo'),
-    # This dollar is ok as it is escaped
-    url(r'^\$', include([
-        url(r'^bar/$', lambda x: x, name='bar'),
-    ])),
-]
