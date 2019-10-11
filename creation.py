aaf913ced9f53fff894902d6eab6518dbfed7059
nesdis <nesdis@gmail.com>
2019-10-11 14:30:06 +0200

    A list of failing tests. Django Version 2.2

--- creation.py
+++ creation.py
@@ -10,23 +10,24 @@ class DatabaseCreation(BaseDatabaseCreation):
         source_database_name = self.connection.settings_dict['NAME']
         target_database_name = self.get_test_db_clone_settings(suffix)['NAME']
         client = self.connection.client_connection
-        args = [
-            'mongodump',
-            '--quiet',
-            '--db',
-            source_database_name
-        ]
-        subprocess.run(args)
-        args = [
-            'mongorestore',
-            f'dump/{source_database_name}',
-            '--quiet',
-            '--db',
-            target_database_name
-        ]
-        subprocess.run(args)
-
-        shutil.rmtree('dump')
         if not keepdb:
-            print('Closing cloned connection')
-            client.close()
+            self._destroy_test_db(target_database_name, verbosity)
+            args = [
+                'mongodump',
+                '--quiet',
+                '--db',
+                source_database_name
+            ]
+            subprocess.run(args)
+            args = [
+                'mongorestore',
+                f'dump/{source_database_name}',
+                '--quiet',
+                '--db',
+                target_database_name
+            ]
+            subprocess.run(args)
+            shutil.rmtree('dump')
+
+        print('Closing cloned connection')
+        client.close()
