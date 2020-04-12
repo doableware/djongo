066693cacc6ec164c6e02766ba2ab2e0c4e0ad9e
nesdis <nesdis@gmail.com>
2020-04-13 00:26:09 +0200

    EmbeddedField, ArrayField, EmbeddedNestedField, EmbeddedInternalField: all tests pass

--- tests/djongo_tests/test_project/xtest_app/admin.py
+++ tests/djongo_tests/test_project/xtest_app/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 from xtest_app.models.array_field import ArrayFieldEntry as ArrayEntry
 from xtest_app.models.basic_field import BasicRelatedEntry, BasicAuthor, BasicBlog
-from xtest_app.models.embedded_field import EmbeddedFieldEntry as EmbeddedEntry, EmbeddedDateEntry
-from xtest_app.models.misc_field import ListEntry, DictEntry
-from xtest_app.models.reference_field import ReferenceEntry, ReferenceAuthor
+from xtest_app.models.embedded_field import EmbeddedFieldEntry as EmbeddedEntry
+# from xtest_app.models.misc_field import ListEntry, DictEntry
+# from xtest_app.models.reference_field import ReferenceEntry, ReferenceAuthor
 
 admin.site.register(BasicAuthor)
 admin.site.register(BasicBlog)
@@ -12,9 +12,9 @@ admin.site.register(ArrayEntry)
 
 admin.site.register(BasicRelatedEntry)
 admin.site.register(EmbeddedEntry)
-admin.site.register(EmbeddedDateEntry)
-admin.site.register(ReferenceEntry)
-admin.site.register(ReferenceAuthor)
-admin.site.register(ListEntry)
-admin.site.register(DictEntry)
+# admin.site.register(EmbeddedDateEntry)
+# admin.site.register(ReferenceEntry)
+# admin.site.register(ReferenceAuthor)
+# admin.site.register(ListEntry)
+# admin.site.register(DictEntry)
 
