df2334a5b6881ef5b1d392227e948e919cdd76e6
nesdis <nesdis@gmail.com>
2020-05-10 23:28:58 +0200

    Support for Django 3.0

--- schema.py
+++ schema.py
@@ -8,28 +8,11 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
 
     sql_create_index = "ALTER TABLE %(table)s ADD CONSTRAINT %(name)s INDEX (%(columns)s)%(extra)s"
     sql_delete_index = "ALTER TABLE %(table)s DROP CONSTRAINT %(name)s INDEX"
-    sql_delete_index2 = "DROP INDEX %(name)s ON %(table)s"
+
 
     def quote_value(self, value):
-        raise NotImplementedError()
+        return value
 
     def prepare_default(self, value):
         raise NotImplementedError()
 
-    # def create_model(self, model):
-    #     db_con = self.connection.connection
-    #     db_con.create_collection(model._meta.db_table)
-    #     logger.debug('Created table {}'.format(model._meta.db_table))
-    #
-    #     for field in model._meta.local_fields:
-    #         if field.get_internal_type() in ("AutoField", "BigAutoField"):
-    #             db_con['__schema__'].\
-    #                 insert_one(
-    #                 {
-    #                     'name': model._meta.db_table,
-    #                     'auto': {
-    #                         'field_name': field.column,
-    #                         'seq': 0
-    #                     }
-    #                 }
-    #             )
