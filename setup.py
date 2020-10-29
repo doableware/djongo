387659cf609f3c4def76059c1fb655b940662cde
johndoe434 <68118162+johndoe434@users.noreply.github.com>
2020-10-29 09:25:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ install_requires = [
     'sqlparse>=0.2.4',
     'pymongo>=3.2.0',
     'django>=2.1',
-    'dnapython>=2.0.0',
+    'dnspython>=2.0',
 ]
 
 if sys.version_info.major == 3 and sys.version_info.minor < 7:
