83103790e2b3f4d4af10a8000b8e236cab3091e4
nesdis <nesdis@gmail.com>
2021-07-06 21:02:15 +0200

    Djongo Cloud

--- docs/assets/js/cookie-consent.js
+++ docs/assets/js/cookie-consent.js
@@ -1,14 +1,1 @@
-// A $( document ).ready() block.
-$( document ).ready(function() {
-    const cookieName = 'user-cookie-consent';
-    let hasConsented = Cookies.get(cookieName);
-
-    if(hasConsented === 'true') {
-       $(".cookie-notice").css('display', 'none');
-    }
-
-    $("#cookie-accept").click(function () {
-        $(".cookie-notice").css('display', 'none');
-        Cookies.set(cookieName, 'true');
-    })
-});
+$(document).ready((function(){const o="user-cookie-consent";"true"===Cookies.get(o)&&$(".cookie-notice").css("display","none"),$("#cookie-accept").click((function(){$(".cookie-notice").css("display","none"),Cookies.set(o,"true")}))}));
--- docs/docs/support/contact/webserver.md
+++
@@ -1,9 +0,0 @@
----
-permalink: /support/contact/webserver/ 
-layout: support-contact
----
-
-{% include form.html 
-    form=site.data.support.tire_form.webserver 
-    next="/support/payment/webserver/"
-    subject="webserver Access" %}
