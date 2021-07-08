5355ce111246fd1447b534b070a682eb399c2d40
Marcus Ong <marcus.ong404@gmail.com>
2021-07-08 14:02:54 +0800

    fix silly bug

--- djongo/utils.py
+++ djongo/utils.py
@@ -14,8 +14,8 @@ def decode_keys(obj, is_key=False):
         return {decode_keys(k, True): decode_keys(v) for k, v in obj.items()}
     if isinstance(obj, list):
         return [decode_keys(v) for v in obj]
-    if (obj.isdigit() or (obj.startswith('-') and obj[1:].isdigit())) and is_key:
-        return str(obj)
+    if isinstance(obj, str) and is_key and (obj.isdigit() or (obj.startswith('-') and obj[1:].isdigit())):
+        return int(obj)
     if isinstance(obj, str) and is_key:
         return obj.replace("\\u002e", ".").replace("\\u0024", "\$").replace("\\\\", "\\")
     return obj
