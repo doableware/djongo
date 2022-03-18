a1d950bfbad5bec16f33970b718a380340ae0610
Floréal Cabanettes <cabanettes.floreal@iuct-oncopole.fr>
2022-03-18 15:10:06 +0100

    Fix bug on parallel tests if mongodb is not on localhost

--- creation.py
+++ creation.py
@@ -9,12 +9,23 @@ class DatabaseCreation(BaseDatabaseCreation):
     def _clone_test_db(self, suffix, verbosity, keepdb=False):
         source_database_name = self.connection.settings_dict['NAME']
         target_database_name = self.get_test_db_clone_settings(suffix)['NAME']
+        try:
+            host = self.connection.settings_dict['CLIENT']['host']
+        except KeyError:
+            host = None
         client = self.connection.client_connection
         if not keepdb:
             self._destroy_test_db(target_database_name, verbosity)
             args = [
                 'mongodump',
                 '--quiet',
+            ]
+            if host is not None:
+                args += [
+                    '--host',
+                    host
+                ]
+            args += [
                 '--db',
                 source_database_name
             ]
@@ -22,7 +33,14 @@ class DatabaseCreation(BaseDatabaseCreation):
             args = [
                 'mongorestore',
                 f'dump/{source_database_name}',
-                '--quiet',
+                '--quiet'
+            ]
+            if host is not None:
+                args += [
+                    '--host',
+                    host
+                ]
+            args += [
                 '--db',
                 target_database_name
             ]
