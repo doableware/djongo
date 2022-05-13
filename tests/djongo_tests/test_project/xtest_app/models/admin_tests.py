272ded4570071211540fcafbc24e973fb20ba847
Gregor Lulic <gregor.lulic@htecgroup.com>
2022-05-13 15:40:09 +0200

    tox

--- tests/djongo_tests/test_project/xtest_app/models/admin_tests.py
+++ tests/djongo_tests/test_project/xtest_app/models/admin_tests.py
@@ -3,7 +3,6 @@ from .basic_field import NamedAuthor, HeadlinedEntry
 
 
 class ArrayFieldEntry(HeadlinedEntry):
-    _id = models.AutoField(primary_key=True)
     authors = models.ArrayField(
         model_container=NamedAuthor
     )
