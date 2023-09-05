9994b626ea26599052f9dba02422c8ed4f49d7ef
movassaghi6 <65920529+movassaghi6@users.noreply.github.com>
2023-09-05 13:18:00 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ def find_version(*file_paths):
 install_requires = [
     'sqlparse==0.4.2',
     'pymongo>=3.6.0',
-    'django>=2.1,<=3.1.3',
+    'django>=2.1,<=4.1.9',
 ]
 
 if sys.version_info.major == 3 and sys.version_info.minor < 7:
